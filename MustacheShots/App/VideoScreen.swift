a84ff4fe57b1c1f9ba5aa4573dec0cf698e0f8f8
Hadia Chaudhary <hadia.chaudhary@devsinc.com>
2023-12-14 14:57:29 +0500

    record screen video and save it in gallery

--- MustacheShots/App/VideoScreen.swift
+++ MustacheShots/App/VideoScreen.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import ReplayKit
+import Photos
+import AVFoundation
+
+extension View {
+  func startRecording(enableMicrophone: Bool = false, completion: @escaping (Error?) -> Void) {
+    let recorder = RPScreenRecorder.shared()
+    recorder.isMicrophoneEnabled = enableMicrophone
+    recorder.startRecording(handler: completion)
+  }
+  func stopRecording() async throws -> URL {
+    let name = UUID().uuidString + ".mov"
+    let url = FileManager.default.temporaryDirectory.appendingPathComponent(name)
+    let recorder = RPScreenRecorder.shared()
+    try await recorder.stopRecording(withOutput: url)
+    return url
+  }
+  func cancelRecording() {
+    let recorder = RPScreenRecorder.shared()
+    recorder.discardRecording {}
+  }
+}
 
 struct VideoScreen: View {
+  @State var isRecording: Bool = false
+  @State var url: URL?
   @State private var isPresented: Bool = false
+  @State var shareVideo: Bool = false
   var body: some View {
-    ARViewContainer().edgesIgnoringSafeArea(.all)
+    ARViewContainer()
+      .edgesIgnoringSafeArea(.all)
       .alert("Face Tracking Unavailable", isPresented: $isPresented) {
         Button {
           isPresented = false
@@ -27,6 +53,81 @@ struct VideoScreen: View {
           isPresented = true
         }
       }
+      .overlay(alignment: .bottomTrailing) {
+        Button {
+          if isRecording {
+            Task {
+              do {
+                self.url = try await stopRecording()
+                isRecording = false
+                shareVideo.toggle()
+                if let url {
+                  saveVideoToAlbum(videoURL: url, albumName: "MyAlbum")
+                }
+              } catch {
+                print(error.localizedDescription)
+              }
+            }
+          } else {
+            startRecording(enableMicrophone: true) { error in
+              if let error = error {
+                print(error.localizedDescription)
+                return
+              }
+              isRecording = true
+            }
+          }
+        } label: {
+          Image(systemName: "record.circle")
+            .font(.largeTitle)
+            .foregroundColor(isRecording ? .red : .black)
+        }
+      }
+  }
+  private func saveVideoToAlbum(videoURL: URL, albumName: String) {
+    if albumExists(albumName: albumName) {
+      let fetchOptions = PHFetchOptions()
+      fetchOptions.predicate = NSPredicate(format: "title = %@", albumName)
+      let collection = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+      if let album = collection.firstObject {
+        saveVideo(videoURL: videoURL, to: album)
+      }
+    } else {
+      var albumPlaceholder: PHObjectPlaceholder?
+      PHPhotoLibrary.shared().performChanges({
+        let createAlbumRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: albumName)
+        albumPlaceholder = createAlbumRequest.placeholderForCreatedAssetCollection
+      }, completionHandler: { success, error in
+        if success {
+          guard let albumPlaceholder = albumPlaceholder else { return }
+          let collectionFetchResult = PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [albumPlaceholder.localIdentifier], options: nil)
+          guard let album = collectionFetchResult.firstObject else { return }
+          self.saveVideo(videoURL: videoURL, to: album)
+        } else {
+          print("Error creating album: \(error?.localizedDescription ?? "")")
+        }
+      })
+    }
+  }
+  private func albumExists(albumName: String) -> Bool {
+    let fetchOptions = PHFetchOptions()
+    fetchOptions.predicate = NSPredicate(format: "title = %@", albumName)
+    let collection = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+    return collection.firstObject != nil
+  }
+  private func saveVideo(videoURL: URL, to album: PHAssetCollection) {
+    PHPhotoLibrary.shared().performChanges({
+      let assetChangeRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: videoURL)
+      let albumChangeRequest = PHAssetCollectionChangeRequest(for: album)
+      let enumeration: NSArray = [assetChangeRequest!.placeholderForCreatedAsset!]
+      albumChangeRequest?.addAssets(enumeration)
+    }, completionHandler: { success, error in
+      if success {
+        print("Successfully saved video to album")
+      } else {
+        print("Error saving video to album: \(error?.localizedDescription ?? "")")
+      }
+    })
   }
 }
 
