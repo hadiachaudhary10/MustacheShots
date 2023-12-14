450d5eead4e84c2e598bea3c7762e43fa2cd8508
Hadia Chaudhary <hadia.chaudhary@devsinc.com>
2023-12-14 12:48:21 +0500

    add mustache on face

--- MustacheShots/App/VideoScreen.swift
+++ MustacheShots/App/VideoScreen.swift
@@ -10,30 +10,41 @@ import RealityKit
 import ARKit
 
 struct VideoScreen: View {
+  @State private var isPresented: Bool = false
   var body: some View {
-    VStack {
-      ARViewContainer()
-        .edgesIgnoringSafeArea(.all)
-    }
-  }
-}
-
-struct CameraScreen_Previews: PreviewProvider {
-  static var previews: some View {
-    VideoScreen()
+    ARViewContainer().edgesIgnoringSafeArea(.all)
+      .alert("Face Tracking Unavailable", isPresented: $isPresented) {
+        Button {
+          isPresented = false
+        } label: {
+          Text("Okay")
+        }
+      } message: {
+        Text("Face tracking requires an iPhone X or later.")
+      }
+      .onAppear {
+        if !ARFaceTrackingConfiguration.isSupported {
+          isPresented = true
+        }
+      }
   }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-  func makeUIView(context: Context) -> some UIView {
+  func makeUIView(context: Context) -> ARView {
     let arView = ARView(frame: .zero)
-    let config = ARFaceTrackingConfiguration()
-    arView.session.run(config)
-    let sphere = ModelEntity(mesh: .generateSphere(radius: 0.08), materials: [SimpleMaterial(color: UIColor.blue, roughness: 0.2, isMetallic: true)])
-    let anchor = AnchorEntity(.face)
-    anchor.addChild(sphere)
-    arView.scene.addAnchor(anchor)
+    if let faceScene = try? Mustache2.load_Mustache2() {
+      arView.scene.anchors.append(faceScene)
+    }
+    let arConfig = ARFaceTrackingConfiguration()
+    arView.session.run(arConfig)
     return arView
   }
-  func updateUIView(_ uiView: UIViewType, context: Context) {}
+  func updateUIView(_ uiView: ARView, context: Context) {}
+}
+
+struct VideoScreen_Previews: PreviewProvider {
+  static var previews: some View {
+    VideoScreen()
+  }
 }
